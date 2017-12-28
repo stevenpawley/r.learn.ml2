39d0f8e9a4464ffb6e03a42d19671290ba224746
Steve <dr.stevenpawley@gmail.com>
2017-12-28 12:19:47 -0700

    Remove starting GRASS externally

--- PygrassML/__init__.py
+++ PygrassML/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
-from ._main import predict, gsession, gvect_to_gpd, gpd_to_gvect
-from .model_selection import specificity_score, cross_val_scores
+from ._main import predict
+from .model_selection import specificity_score, cross_val_scores8
 from .prediction import predict
 from .sampling import extract_pixels, extract_points
 from .utils import save_training_data, load_training_data, save_model, load_model
