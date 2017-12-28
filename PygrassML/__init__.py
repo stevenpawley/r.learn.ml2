fc7ed9c431b1677b43df7f0e9e6974772ff048ba
Steve <dr.stevenpawley@gmail.com>
2017-12-28 12:21:40 -0700

    Fix typo

--- PygrassML/__init__.py
+++ PygrassML/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from ._main import predict
-from .model_selection import specificity_score, cross_val_scores8
+from .model_selection import specificity_score, cross_val_scores
 from .prediction import predict
 from .sampling import extract_pixels, extract_points
 from .utils import save_training_data, load_training_data, save_model, load_model
