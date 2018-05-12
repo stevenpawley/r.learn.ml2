1d2b3a2429e51b51a5d5c0d52b335edc21b47d75
Steve <dr.stevenpawley@gmail.com>
2018-05-11 22:41:18 -0600

    Fixed encoding issue

--- PygrassML/test.py
+++ PygrassML/test.py
@@ -6,6 +6,8 @@ Created on Thu Mar 15 23:32:03 2018
 @author: steven
 """
 
+from PygrassML import RasterStack
+
 stack = RasterStack(['lsat7_2002_10', 'lsat7_2002_20', 'lsat7_2002_30', 'lsat7_2002_40', 'lsat7_2002_50', 'lsat7_2002_70'])
 X, y, cds = stack.extract_points(vect_name='landclass96_roi@PERMANENT', field='id', na_rm=True)
 X, y, cds = stack.extract_pixels('landclass96_roi@PERMANENT')
@@ -20,7 +22,7 @@ clf.fit(X, y)
 stack.predict(estimator=clf, output='test', overwrite=True)
 stack.predict_proba(estimator=clf, output='test1', overwrite=True)
 
-
+import grass.script as gs
 from sklearn.model_selection import cross_validate, cross_val_predict
 from sklearn.metrics import confusion_matrix
 scores = cross_validate(clf, X, y)
